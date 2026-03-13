4d3dcafe5c7d93da3733037cc025c4d77f2635d3
sheikhahmadullah <sheikhahmadullah60@gmail.com>
2026-03-13 22:58:39 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+
+
+const express = require("express");
+
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("Leetcode backend running...");
+
+})
+
+
+app.listen(8000,()=> console.log("Server running on port 8000"))
